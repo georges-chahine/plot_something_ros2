862df94af3cba65f055f95aae267cf105da56811
Georges Chahine <chahine.georges@gmail.com>
2022-09-23 10:12:35 +0200

    first push

---
+++ plot_ros2.py
@@ -0,0 +1,14 @@
+from rosbags.rosbag2 import Reader
+from rosbags.serde import deserialize_cdr
+
+# create reader instance and open for reading
+with Reader('/home/georges/Downloads/Data IT3 19sept/VF_04_dep_SE-1663603086') as reader:
+    # topic and msgtype information is available on .connections list
+    for connection in reader.connections:
+        print(connection.topic, connection.msgtype)
+
+    # iterate over messages
+    for connection, timestamp, rawdata in reader.messages():
+        if connection.topic == '/tf':
+            msg = deserialize_cdr(rawdata, connection.msgtype)
+            print(msg.transforms[0].transform.translation.x)
